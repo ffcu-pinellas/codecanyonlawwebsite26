4df6395ae3038f854fde6e21538d394e39284a98
Arcade12009 <elainemarti009@gmail.com>
2026-06-10 23:41:23 +0100

    Add programmatic migrate support to setup endpoint

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/setup', function (){
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
     \Illuminate\Support\Facades\Artisan::call('storage:link');
     \Illuminate\Support\Facades\Artisan::call('cache:clear');
     \Illuminate\Support\Facades\Artisan::call('view:clear');
