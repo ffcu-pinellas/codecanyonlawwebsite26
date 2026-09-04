5df35901d505294e455586882c0a7472bb01997a
Arcade12009 <elainemarti009@gmail.com>
2026-09-04 15:34:16 +0100

    Fix missing Auth facade import in UserController and AdminDocumentTemplateController

--- app/Http/Controllers/AdminControllers/AdminDocumentTemplateController.php
+++ app/Http/Controllers/AdminControllers/AdminDocumentTemplateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\AdminControllers;
 use App\Http\Controllers\Controller;
 use App\Models\DocumentTemplate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AdminDocumentTemplateController extends Controller
